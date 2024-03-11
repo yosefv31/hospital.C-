86a03ca027ad0b0af2472882dc993b51900891af
yosefv31 <yosefvargas25@gmail.com>
2024-03-10 19:13:39 -0600

    Creando main del programa
    Inicios del programa

---
+++ Hospital/Hospital/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Hospital
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            bool booleano = true;
+            while (booleano)
+            {
+                Console.WriteLine("Menú");
+                Console.WriteLine("1) Registrar entrada");
+                Console.WriteLine("2) Registrar salida");
+                Console.WriteLine("3) Consultar cédulas");
+                Console.WriteLine("4) Consultar cédula especifica");
+                Console.WriteLine("5) Salir");
+                try
+                {
+                    Console.WriteLine("Digite una opcion: ");
+                    byte opcionUsuario = Convert.ToByte(Console.ReadLine());
+                    switch (opcionUsuario)
+                    {
+                        case 1:
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            Console.WriteLine("Gracias por utilizar el programa :)");
+                            booleano = false;
+                            break;
+                    }
+                }
+                catch (SyntaxErrorException)
+                {
+                    Console.WriteLine("Opcion invalida!!!");
+                    Console.WriteLine("Porfavor digite una opcion valida ");
+                }
+            }
+        }
+    }
+}
