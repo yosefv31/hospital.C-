fb015fd8edcadab2d0d9c27870c2f1623a07a98f
yosefv31 <yosefvargas25@gmail.com>
2024-03-13 21:25:24 -0600

    Avance de clases
    Se logra pulir la clase "registroentrada" y se empieza a trabajar en la clase "registro salida"

---
+++ Hospital/Hospital/Hospital/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Hospital
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {           
+            bool booleano = true;
+            while (booleano)
+            {
+                Console.WriteLine("Menú");
+                Console.WriteLine("1) Registrar entrada");
+                Console.WriteLine("2) Registrar salida");
+                Console.WriteLine("3) Consultar cédulas");
+                Console.WriteLine("4) Consultar cédula especifica");
+                Console.WriteLine("5) Salir");
+                try
+                {                    
+                    Console.WriteLine("Digite una opcion: ");
+                    byte opcionUsuario = Convert.ToByte(Console.ReadLine());
+                    
+                    switch (opcionUsuario)
+                    {
+                        case 1:
+                            registroEntrada login = new registroEntrada("", "", 0, 0);
+                            Console.WriteLine("Digite el nombre del usuario a registrar: ");
+                            login.nombre = Console.ReadLine();
+                            login.entrada = Convert.ToString(DateTime.Now);
+                            Console.WriteLine("Digite el ID del usuario: ");
+                            login.id = Convert.ToInt32(Console.ReadLine());
+                            Console.WriteLine("Digite la edad del usuario: ");
+                            login.edad = Convert.ToByte(Console.ReadLine());
+                            login.str();
+                            login.agregarListas(login.nombre, login.entrada, login.id, login.edad);
+                            login.impresion();
+                            break;
+
+                        case 2:
+                            registroSalida logout = new registroSalida("","",0,0,"",0);
+                            Console.WriteLine("Digite la cédula para el registro de salida: ");
+                            logout.idValue = Convert.ToInt32(Console.ReadLine());
+                            logout.salida = Convert.ToString(DateTime.Now);
+                            logout.agregarSalidas(logout.salida);
+                            logout.imprimi();
+                            
+                            break;
+                        case 3:
+                            break;
+                        case 4:
+                            break;
+                        case 5:
+                            Console.WriteLine("Gracias por utilizar el programa :)");
+                            booleano = false;
+                            break;
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Opcion invalida!!!");
+                    Console.WriteLine("Porfavor digite una opcion valida ");
+                }
+
+                catch (OverflowException e) 
+                {
+                    Console.WriteLine($"Error: {e}");
+                }
+
+            }
+        }
+    }
+}
---
+++ Hospital/Hospital/Hospital/registroSalida.cs
@@ -0,0 +1,60 @@
+﻿using Hospital;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+	public class registroSalida : registroEntrada
+	{
+		//Atributos
+		public string salida {  get; set; }
+		public int idValue { get; set; }
+
+		//Constructor 
+		public registroSalida(string nombre, string entrada, int id, byte edad, string salida, int idValue) : base(nombre, entrada, id, edad)
+		{
+			this.salida = salida;
+            this.idValue = idValue;
+		}
+
+		//Métodos externos 
+		public void agregarSalidas(string elemento1) 
+		{ 
+			byte i = 0;
+			foreach (int x in Id) 
+			{
+                if (idValue == x) 
+				{
+					int indice = Id.IndexOf(x);
+					salidas.Insert(indice, elemento1);
+					salidas.Remove("NA");
+					Console.WriteLine("La salida del usuario fue registrada con éxito!!!");
+					break;
+				}
+				else if (i >= Id.Count && x!= idValue)
+				{
+					Console.WriteLine("El usuario no fue encontrado!!!");
+					break;
+				}
+				else if (salidas.Count <= Id.Count)
+				{
+					salidas.Add("NA");
+				}
+				i++;
+            }
+        }
+		public void imprimi() 
+		{
+            foreach (string x in salidas)
+            {
+                Console.WriteLine(x);
+            }
+        }
+		
+	}
+}
